21ffdf1d4611a5bf51b236ede4fdcd0b8304e514
Hector A. Reyes <hreyes@amwtexas.com>
2020-10-31 16:40:02 -0600

    Created simple structure for all threads needed

---
+++ videoPlayer.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+from threading import Thread, Semaphore
+import cv2,os,time,sys
+
+def main():
+    return
+
+class extract(Thread):
+    def __init__(self,file):
+        Thread.__init__(self)
+        self.filename = file
+    
+    def run(self):
+        return
+
+class convert(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        return
+
+class display(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        return
+
+main()
