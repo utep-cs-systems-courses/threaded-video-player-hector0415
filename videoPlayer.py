11763b86264aa661c1e05eedd82f5d575eb54382
Hector A. Reyes <hreyes@amwtexas.com>
2020-11-08 20:25:38 -0700

    Created new queue object and implemented into thread methods

--- videoPlayer.py
+++ videoPlayer.py
@@ -1,36 +1,28 @@
 #! /usr/bin/env python3
 
-from threading import Thread, Semaphore
+from threading import Thread, Semaphore, Lock
 import cv2,os,time,sys
 
-sem = Semaphore()
-rawFrames = []
-grayFrames = []
-queueLimit = 10
-
-def main():
-    if len(sys.argv) != 2:
-        print("No video name was given, run program as below")
-        print("python3 videoPlayer.py <video file name>")
-        sys.exit(0)
-    else: #correct number of arguments given
-        if not os.path.isfile(sys.argv[1]):
-            print("Valid video was not found, run program as below")
-            print("python3 videoPlayer.py <video file name>")
-            sys.exit(0)
-        else: #valid file given
-            fileName = sys.argv[1]
-            try:
-                video = cv2.VideoCapture(fileName)
-                extract_thread = extract(video)
-                extract_thread.start()
-                convert_thread = convert()
-                convert_thread.start()
-                display_thread = display()
-                display_thread.start()
-            except:
-                print("Something went wrong!")
-            
+class Queue():
+    def __init__(self,capacity = 10):
+        self.queue = []
+        self.lock = Lock()
+        self.full = Semaphore(0)
+        self.empty = Semaphore(capacity)
+    
+    def put(self,frame):
+        self.empty.acquire()
+        self.lock.acquire()
+        self.queue.append(frame)
+        self.lock.release()
+        self.full.release()
+    
+    def get(self):
+        self.full.acquire()
+        self.lock.acquire()
+        frame = self.queue.pop(0)
+        self.lock.release()
+        self.empty.release()
 
 class extract(Thread):
     def __init__(self,video):
@@ -41,14 +33,9 @@ class extract(Thread):
         vid = self.video
         successful, frame = vid.read()
         while successful:
-            if len(rawFrames) <= queueLimit:
-                sem.acquire()
-                rawFrames.append(frame)
-                sem.release()
-                successful, frame = vid.read()
-        sem.acquire()
-        rawFrames.append(None)
-        sem.release()
+            rawFrames.put(frame)
+            successful, frame = vid.read()
+        rawFrames.put(None)
         return
 
 class convert(Thread):
@@ -57,21 +44,12 @@ class convert(Thread):
 
     def run(self):
         while True:
-           if len(rawFrames) > 0 and len(grayFrames) <= queueLimit:
-                sem.acquire()
-                frame = rawFrames.pop(0)
-                sem.release()
-
-                if frame is None:
-                    sem.acquire()
-                    grayFrames.append(None)
-                    sem.release()
-                    return
-                
-                grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                sem.acquire()
-                grayFrames.append(grayFrame)
-                sem.release()
+            frame = rawFrames.get()
+            if frame is None:
+                grayFrames.append(None)
+                return
+            grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            grayFrames.put(grayFrame)
         return
 
 class display(Thread):
@@ -81,20 +59,37 @@ class display(Thread):
 
     def run(self):
         while True:
-            if len(grayFrames) > 0:
-                sem.acquire()
-                frame = grayFrames.pop(0)
-                sem.release()
-
-                if frame is None:
-                    break
-                
-                cv2.imshow('Video',frame)
-
-                if cv2.waitKey(self.delay) and 0xFF == ord("q"):
-                    break
-
+            frame = grayFrames.get()
+            if frame is None:
+                break
+            cv2.imshow('Video',frame)
+            if cv2.waitKey(self.delay) and 0xFF == ord("q"):
+                break
         cv2.destroyAllWindows()
         return
 
-main()
+queueLimit = 10
+rawFrames = Queue(queueLimit)
+grayFrames = Queue(queueLimit)
+
+if len(sys.argv) != 2:
+    print("No video name was given, run program as below")
+    print("python3 videoPlayer.py <video file name>")
+    sys.exit(0)
+else: #correct number of arguments given
+    if not os.path.isfile(sys.argv[1]):
+        print("Valid video was not found, run program as below")
+        print("python3 videoPlayer.py <video file name>")
+        sys.exit(0)
+    else: #valid file given
+        fileName = sys.argv[1]
+        try:
+            video = cv2.VideoCapture(fileName)
+            extract_thread = extract(video)
+            extract_thread.start()
+            convert_thread = convert()
+            convert_thread.start()
+            display_thread = display()
+            display_thread.start()
+        except:
+            print("Something went wrong!")
